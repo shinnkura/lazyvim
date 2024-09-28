6849263f99c73f7fd29457f55ab3f373cafc4187
shinnkura <n.shinzo1010@icloud.com>
2024-09-29 00:03:47 +0900

    feat(keymap): 書き方修正

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,56 +16,56 @@ cmp.setup({
   },
 })
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Esc><Esc>", ":nohlsearch<CR><Esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR><Esc>", { noremap = true, silent = true })
 
 -- インサートモードでのカーソル移動の設定
-vim.api.nvim_set_keymap("i", "<C-b>", "<Left>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-f>", "<Right>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-p>", "<Up>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-n>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-b>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-f>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-p>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-n>", "<Down>", { noremap = true, silent = true })
 -- Ctrl+aで行頭、Ctrl+eで行末に移動
-vim.api.nvim_set_keymap("i", "<C-a>", "<Home>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-e>", "<End>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-a>", "<Home>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-e>", "<End>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-d>", "<C-o>dl", { noremap = true, silent = true })
 
 -- カーソル移動の高速化
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "n", "nzzzv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "N", "Nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
 
 -- ノーマルモードでのカーソル移動
-vim.api.nvim_set_keymap("n", "<C-b>", "h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-f>", "l", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-p>", "k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-n>", "j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-a>", "0", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-e>", "$", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-b>", "h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-f>", "l", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-p>", "k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", "j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-a>", "0", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-e>", "$", { noremap = true, silent = true })
 
 -- ビジュアルモードでのカーソル移動
-vim.api.nvim_set_keymap("v", "<C-b>", "h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-f>", "l", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-p>", "k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-n>", "j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-a>", "0", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-e>", "$", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-b>", "h", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-f>", "l", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-p>", "k", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-n>", "j", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-a>", "0", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-e>", "$", { noremap = true, silent = true })
 
 -- コマンドモードでのカーソル移動
-vim.api.nvim_set_keymap("c", "<C-b>", "<Left>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("c", "<C-f>", "<Right>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("c", "<C-a>", "<Home>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("c", "<C-e>", "<End>", { noremap = true, silent = true })
+vim.keymap.set("c", "<C-b>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("c", "<C-f>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true, silent = true })
+vim.keymap.set("c", "<C-e>", "<End>", { noremap = true, silent = true })
 
 -- 括弧の自動補完
-vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", '"', '""<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "'", "''<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "(", "()<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "{", "{}<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "[", "[]<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", '"', '""<Left>', { noremap = true, silent = true })
+vim.keymap.set("i", "'", "''<Left>", { noremap = true, silent = true })
 
 -- Telescopeの設定
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true })
